60f21c79c315d15da0f7856bfa012d2986f445ac
frostbane <frostbane@programmer.net>
2021-08-30 03:02:51 +1000

    link winsock2

--- socket.cpp
+++ socket.cpp
@@ -1,12 +1,36 @@
 #include "socket.hpp"
 
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 using namespace nt::http;
 
 Socket::Socket()
 {
-    std::cout << "socket" << std::endl;
 #ifdef LOSER
+    int     ret;
+    WSADATA wsaData;
+
+    if ((ret = WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
+        std::string error = "WSAStartup failed with error " + ret;
+
+        throw std::runtime_error(error.c_str());
+    }
+#endif
+
+    std::cout << "socket" << std::endl;
+}
+
+Socket::~Socket()
+{
+#ifdef LOSER
+    if (WSACleanup() == SOCKET_ERROR) {
+        int         ret   = WSAGetLastError();
+        std::string error = "WSACleanup failed with error " + ret;
+
+        std::cerr << error << std::endl;
+        // throw std::runtime_error(error.c_str());
+    }
 #endif
 }
