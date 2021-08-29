8c1f90c3a118d61c620a70a9bd2290b88580d720
frostbane <frostbane@programmer.net>
2021-08-30 02:32:24 +1000

    initial commit

---
+++ main.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "socket.hpp"
+
+int
+main(int, char**)
+{
+    nt::http::Socket socket;
+
+    return EXIT_SUCCESS;
+}
---
+++ socket.cpp
@@ -0,0 +1,12 @@
+#include "socket.hpp"
+
+#include <iostream>
+
+using namespace nt::http;
+
+Socket::Socket()
+{
+    std::cout << "socket" << std::endl;
+#ifdef LOSER
+#endif
+}
