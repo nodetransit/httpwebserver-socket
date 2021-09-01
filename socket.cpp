3cd1ae827d1bdbdb4d60c9360e52bbab7a20e103
frostbane <frostbane@programmer.net>
2021-09-01 15:40:40 +1000

    refactor error message handling

--- socket.cpp
+++ socket.cpp
@@ -6,19 +6,276 @@
 
 using namespace nt::http;
 
+namespace {
+const std::string
+_get_last_error()
+{
+#ifdef LOSER
+    int error = ::WSAGetLastError();
+#elif defined(LINUX)
+    int error = errno;
+#endif
+
+    switch (error) {
+#ifdef LOSER
+    case WSA_INVALID_HANDLE: return "Specified event object handle is invalid.";
+    case WSA_NOT_ENOUGH_MEMORY: return "Insufficient memory available.";
+    case WSA_INVALID_PARAMETER: return "One or more parameters are invalid.";
+    case WSA_OPERATION_ABORTED: return "Overlapped operation aborted.";
+    case WSA_IO_INCOMPLETE: return "Overlapped I/O event object not in signaled state.";
+    case WSA_IO_PENDING: return "Overlapped operations will complete later.";
+    case WSAEINTR: return "Interrupted function call.";
+    case WSAEBADF: return "File handle is not valid.";
+    case WSAEACCES: return "Permission denied.";
+    case WSAEFAULT: return "Bad address.";
+    case WSAEINVAL: return "Invalid argument.";
+    case WSAEMFILE: return "Too many open files.";
+    case WSAEWOULDBLOCK: return "Resource temporarily unavailable.";
+    case WSAEINPROGRESS: return "Operation now in progress.";
+    case WSAEALREADY: return "Operation already in progress.";
+    case WSAENOTSOCK: return "Socket operation on nonsocket.";
+    case WSAEDESTADDRREQ: return "Destination address required.";
+    case WSAEMSGSIZE: return "Message too long.";
+    case WSAEPROTOTYPE: return "Protocol wrong type for socket.";
+    case WSAENOPROTOOPT: return "Bad protocol option.";
+    case WSAEPROTONOSUPPORT: return "Protocol not supported.";
+    case WSAESOCKTNOSUPPORT: return "Socket type not supported.";
+    case WSAEOPNOTSUPP: return "Operation not supported.";
+    case WSAEPFNOSUPPORT: return "Protocol family not supported.";
+    case WSAEAFNOSUPPORT: return "Address family not supported by protocol family.";
+    case WSAEADDRINUSE: return "Address already in use.";
+    case WSAEADDRNOTAVAIL: return "Cannot assign requested address.";
+    case WSAENETDOWN: return "Network is down.";
+    case WSAENETUNREACH: return "Network is unreachable.";
+    case WSAENETRESET: return "Network dropped connection on reset.";
+    case WSAECONNABORTED: return "Software caused connection abort.";
+    case WSAECONNRESET: return "Connection reset by peer.";
+    case WSAENOBUFS: return "No buffer space available.";
+    case WSAEISCONN: return "Socket is already connected.";
+    case WSAENOTCONN: return "Socket is not connected.";
+    case WSAESHUTDOWN: return "Cannot send after socket shutdown.";
+    case WSAETOOMANYREFS: return "Too many references.";
+    case WSAETIMEDOUT: return "Connection timed out.";
+    case WSAECONNREFUSED: return "Connection refused.";
+    case WSAELOOP: return "Cannot translate name.";
+    case WSAENAMETOOLONG: return "Name too long.";
+    case WSAEHOSTDOWN: return "Host is down.";
+    case WSAEHOSTUNREACH: return "No route to host.";
+    case WSAENOTEMPTY: return "Directory not empty.";
+    case WSAEPROCLIM: return "Too many processes.";
+    case WSAEUSERS: return "User quota exceeded.";
+    case WSAEDQUOT: return "Disk quota exceeded.";
+    case WSAESTALE: return "Stale file handle reference.";
+    case WSAEREMOTE: return "Item is remote.";
+    case WSASYSNOTREADY: return "Network subsystem is unavailable.";
+    case WSAVERNOTSUPPORTED: return "Winsock.dll version out of range.";
+    case WSANOTINITIALISED: return "Successful WSAStartup not yet performed.";
+    case WSAEDISCON: return "Graceful shutdown in progress.";
+    case WSAENOMORE: return "No more results.";
+    case WSAECANCELLED: return "Call has been canceled.";
+    case WSAEINVALIDPROCTABLE: return "Procedure call table is invalid.";
+    case WSAEINVALIDPROVIDER: return "Service provider is invalid.";
+    case WSAEPROVIDERFAILEDINIT: return "Service provider failed to initialize.";
+    case WSASYSCALLFAILURE: return "System call failure.";
+    case WSASERVICE_NOT_FOUND: return "Service not found.";
+    case WSATYPE_NOT_FOUND: return "Class type not found.";
+    case WSA_E_NO_MORE: return "No more results.";
+    case WSA_E_CANCELLED: return "Call was canceled.";
+    case WSAEREFUSED: return "Database query was refused.";
+    case WSAHOST_NOT_FOUND: return "Host not found.";
+    case WSATRY_AGAIN: return "Nonauthoritative host not found.";
+    case WSANO_RECOVERY: return "This is a nonrecoverable error.";
+    case WSANO_DATA: return "Valid name, no data record of requested type.";
+    case WSA_QOS_RECEIVERS: return "QoS receivers.";
+    case WSA_QOS_SENDERS: return "QoS senders.";
+    case WSA_QOS_NO_SENDERS: return "No QoS senders.";
+    case WSA_QOS_NO_RECEIVERS: return "QoS no receivers.";
+    case WSA_QOS_REQUEST_CONFIRMED: return "QoS request confirmed.";
+    case WSA_QOS_ADMISSION_FAILURE: return "QoS admission error.";
+    case WSA_QOS_POLICY_FAILURE: return "QoS policy failure.";
+    case WSA_QOS_BAD_STYLE: return "QoS bad style.";
+    case WSA_QOS_BAD_OBJECT: return "QoS bad object.";
+    case WSA_QOS_TRAFFIC_CTRL_ERROR: return "QoS traffic control error.";
+    case WSA_QOS_GENERIC_ERROR: return "QoS generic error.";
+    case WSA_QOS_ESERVICETYPE: return "QoS service type error.";
+    case WSA_QOS_EFLOWSPEC: return "QoS flowspec error.";
+    case WSA_QOS_EPROVSPECBUF: return "Invalid QoS provider buffer.";
+    case WSA_QOS_EFILTERSTYLE: return "Invalid QoS filter style.";
+    case WSA_QOS_EFILTERTYPE: return "Invalid QoS filter type.";
+    case WSA_QOS_EFILTERCOUNT: return "Incorrect QoS filter count.";
+    case WSA_QOS_EOBJLENGTH: return "Invalid QoS object length.";
+    case WSA_QOS_EFLOWCOUNT: return "Incorrect QoS flow count.";
+    case WSA_QOS_EUNKOWNPSOBJ: return "Unrecognized QoS object.";
+    case WSA_QOS_EPOLICYOBJ: return "Invalid QoS policy object.";
+    case WSA_QOS_EFLOWDESC: return "Invalid QoS flow descriptor.";
+    case WSA_QOS_EPSFLOWSPEC: return "Invalid QoS provider-specific flowspec.";
+    case WSA_QOS_EPSFILTERSPEC: return "Invalid QoS provider-specific filterspec.";
+    case WSA_QOS_ESDMODEOBJ: return "Invalid QoS shape discard mode object.";
+    case WSA_QOS_ESHAPERATEOBJ: return "Invalid QoS shaping rate object.";
+    case WSA_QOS_RESERVED_PETYPE: return "Reserved policy QoS element type.";
+#elif defined(LINUX)
+    case EPERM: return "Operation not permitted";
+    case ENOENT: return "No such file or directory";
+    case ESRCH: return "No such process";
+    case EINTR: return "Interrupted system call";
+    case EIO: return "I/O error";
+    case ENXIO: return "No such device or address";
+    case E2BIG: return "Argument list too long";
+    case ENOEXEC: return "Exec format error";
+    case EBADF: return "Bad file number";
+    case ECHILD: return "No child processes";
+    // case EAGAIN: return "Try again";
+    case ENOMEM: return "Out of memory";
+    case EACCES: return "Permission denied";
+    case EFAULT: return "Bad address";
+    case ENOTBLK: return "Block device required";
+    case EBUSY: return "Device or resource busy";
+    case EEXIST: return "File exists";
+    case EXDEV: return "Cross-device link";
+    case ENODEV: return "No such device";
+    case ENOTDIR: return "Not a directory";
+    case EISDIR: return "Is a directory";
+    case EINVAL: return "Invalid argument";
+    case ENFILE: return "File table overflow";
+    case EMFILE: return "Too many open files";
+    case ENOTTY: return "Not a typewriter";
+    case ETXTBSY: return "Text file busy";
+    case EFBIG: return "File too large";
+    case ENOSPC: return "No space left on device";
+    case ESPIPE: return "Illegal seek";
+    case EROFS: return "Read-only file system";
+    case EMLINK: return "Too many links";
+    case EPIPE: return "Broken pipe";
+    case EDOM: return "Math argument out of domain of func";
+    case ERANGE: return "Math result not representable";
+    case EDEADLK: return "Resource deadlock would occur";
+    case ENAMETOOLONG: return "File name too long";
+    case ENOLCK: return "No record locks available";
+    case ENOSYS: return "Invalid system call number";
+    case ENOTEMPTY: return "Directory not empty";
+    case ELOOP: return "Too many symbolic links encountered";
+    case EWOULDBLOCK: return "Operation would block";
+    case ENOMSG: return "No message of desired type";
+    case EIDRM: return "Identifier removed";
+    case ECHRNG: return "Channel number out of range";
+    case EL2NSYNC: return "Level 2 not synchronized";
+    case EL3HLT: return "Level 3 halted";
+    case EL3RST: return "Level 3 reset";
+    case ELNRNG: return "Link number out of range";
+    case EUNATCH: return "Protocol driver not attached";
+    case ENOCSI: return "No CSI structure available";
+    case EL2HLT: return "Level 2 halted";
+    case EBADE: return "Invalid exchange";
+    case EBADR: return "Invalid request descriptor";
+    case EXFULL: return "Exchange full";
+    case ENOANO: return "No anode";
+    case EBADRQC: return "Invalid request code";
+    case EBADSLT: return "Invalid slot";
+    case EBFONT: return "Bad font file format";
+    case ENOSTR: return "Device not a stream";
+    case ENODATA: return "No data available";
+    case ETIME: return "Timer expired";
+    case ENOSR: return "Out of streams resources";
+    case ENONET: return "Machine is not on the network";
+    case ENOPKG: return "Package not installed";
+    case EREMOTE: return "Object is remote";
+    case ENOLINK: return "Link has been severed";
+    case EADV: return "Advertise error";
+    case ESRMNT: return "Srmount error";
+    case ECOMM: return "Communication error on send";
+    case EPROTO: return "Protocol error";
+    case EMULTIHOP: return "Multihop attempted";
+    case EDOTDOT: return "RFS specific error";
+    case EBADMSG: return "Not a data message";
+    case EOVERFLOW: return "Value too large for defined data type";
+    case ENOTUNIQ: return "Name not unique on network";
+    case EBADFD: return "File descriptor in bad state";
+    case EREMCHG: return "Remote address changed";
+    case ELIBACC: return "Can not access a needed shared library";
+    case ELIBBAD: return "Accessing a corrupted shared library";
+    case ELIBSCN: return ".lib section in a.out corrupted";
+    case ELIBMAX: return "Attempting to link in too many shared libraries";
+    case ELIBEXEC: return "Cannot exec a shared library directly";
+    case EILSEQ: return "Illegal byte sequence";
+    case ERESTART: return "Interrupted system call should be restarted";
+    case ESTRPIPE: return "Streams pipe error";
+    case EUSERS: return "Too many users";
+    case ENOTSOCK: return "Socket operation on non-socket";
+    case EDESTADDRREQ: return "Destination address required";
+    case EMSGSIZE: return "Message too long";
+    case EPROTOTYPE: return "Protocol wrong type for socket";
+    case ENOPROTOOPT: return "Protocol not available";
+    case EPROTONOSUPPORT: return "Protocol not supported";
+    case ESOCKTNOSUPPORT: return "Socket type not supported";
+    case EOPNOTSUPP: return "Operation not supported on transport endpoint";
+    case EPFNOSUPPORT: return "Protocol family not supported";
+    case EAFNOSUPPORT: return "Address family not supported by protocol";
+    case EADDRINUSE: return "Address already in use";
+    case EADDRNOTAVAIL: return "Cannot assign requested address";
+    case ENETDOWN: return "Network is down";
+    case ENETUNREACH: return "Network is unreachable";
+    case ENETRESET: return "Network dropped connection because of reset";
+    case ECONNABORTED: return "Software caused connection abort";
+    case ECONNRESET: return "Connection reset by peer";
+    case ENOBUFS: return "No buffer space available";
+    case EISCONN: return "Transport endpoint is already connected";
+    case ENOTCONN: return "Transport endpoint is not connected";
+    case ESHUTDOWN: return "Cannot send after transport endpoint shutdown";
+    case ETOOMANYREFS: return "Too many references: cannot splice";
+    case ETIMEDOUT: return "Connection timed out";
+    case ECONNREFUSED: return "Connection refused";
+    case EHOSTDOWN: return "Host is down";
+    case EHOSTUNREACH: return "No route to host";
+    case EALREADY: return "Operation already in progress";
+    case EINPROGRESS: return "Operation now in progress";
+    case ESTALE: return "Stale file handle";
+    case EUCLEAN: return "Structure needs cleaning";
+    case ENOTNAM: return "Not a XENIX named type file";
+    case ENAVAIL: return "No XENIX semaphores available";
+    case EISNAM: return "Is a named type file";
+    case EREMOTEIO: return "Remote I/O error";
+    case EDQUOT: return "Quota exceeded";
+    case ENOMEDIUM: return "No medium found";
+    case EMEDIUMTYPE: return "Wrong medium type";
+    case ECANCELED: return "Operation Canceled";
+    case ENOKEY: return "Required key not available";
+    case EKEYEXPIRED: return "Key has expired";
+    case EKEYREVOKED: return "Key has been revoked";
+    case EKEYREJECTED: return "Key was rejected by service";
+    case EOWNERDEAD: return "Owner died";
+    case ENOTRECOVERABLE: return "State not recoverable";
+    case ERFKILL: return "Operation not possible due to RF-kill";
+    case EHWPOISON: return "Memory page has hardware error";
+#endif
+    default: return "Error code: " + std::to_string(error);
+    }
+}
+
+const std::string
+_get_last_error(const std::string& prefix)
+{
+    return prefix + " " + _get_last_error();
+}
+}
+
 Socket::Socket() :
     port(0),
     connection_count(0),
-#ifdef LOSER
-    socket(NULL),
-#elif defined(LINUX)
-      socket(0),
-#endif
+    socket(0),
     is_open(false)
 {
-    create_socket();
+#ifdef LOSER
+    int     ret;
+    WSADATA wsaData;
 
-    std::cout << "creating socket" << std::endl;
+    if ((ret = ::WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
+        std::string error = _get_last_error("Failed to start up.");
+
+        throw std::runtime_error(error.c_str());
+    }
+#endif
+
+    create_socket();
 }
 
 Socket::~Socket() noexcept
@@ -29,8 +286,7 @@ Socket::~Socket() noexcept
 
 #ifdef LOSER
     if (::WSACleanup() == SOCKET_ERROR) {
-        int         ret   = WSAGetLastError();
-        std::string error = "WSACleanup failed with error " + ret;
+        std::string error = _get_last_error("Failed to clean up.");
 
         std::cerr << error << std::endl;
         // throw std::runtime_error(error.c_str());
@@ -42,37 +298,15 @@ void
 Socket::create_socket()
 {
 #ifdef LOSER
-    int     ret;
-    WSADATA wsaData;
-
-    if ((ret = ::WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
-        std::string error = "WSAStartup failed with error " + ret;
-
-        throw std::runtime_error(error.c_str());
-    }
-
     socket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
 #elif defined(LINUX)
     socket = ::socket(AF_INET, SOCK_STREAM, 0);
 #endif
 
     if (socket == INVALID_SOCKET) {
-#ifdef LOSER
-        int error = ::WSAGetLastError();
-        // todo error handling
-        throw std::runtime_error("Invalid socket");
-#elif defined(LINUX)
-        std::string error = "fail to create socket";
-
-        switch (errno) {
-            // todo get error message
-        default:
-            break;
-        }
+        std::string error = _get_last_error("Failed to create socket.");
 
         throw std::runtime_error(error.c_str());
-#endif
     }
 
     static const int ONE = 1;
@@ -84,10 +318,9 @@ Socket::create_socket()
 #endif
 
     if(setopt_result == SOCKET_ERROR) {
-#ifdef LOSER
-#elif defined(LINUX)
-#endif
-        throw std::runtime_error("unable to set socket option");
+        std::string error = _get_last_error("Failed to set socket option.");
+
+        throw std::runtime_error(error.c_str());
     }
 
     is_open = true;
@@ -105,23 +338,9 @@ Socket::bind(const unsigned short p)
     server_address.sin_addr.s_addr = ::htonl(INADDR_ANY);
 
     if (::bind(socket, (sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
-#ifdef LOSER
-        int error = ::WSAGetLastError();
-        // todo error handling
-        throw std::runtime_error("Invalid socket");
-#elif defined(LINUX)
-        std::string error = "fail to bind socket";
-
-        switch (errno) {
-            // todo get error message
-        default:
-            break;
-        }
-
-        error += " " + std::to_string(errno);
+        std::string error = _get_last_error("Failed to bind to port " + std::to_string(port) + ".");
 
         throw std::runtime_error(error.c_str());
-#endif
     }
 }
 
@@ -133,24 +352,9 @@ Socket::listen(const unsigned int count, event_callback callback)
     int listen_result = ::listen(socket, connection_count);
 
     if(listen_result == SOCKET_ERROR) {
-#ifdef LOSER
-        int error = ::WSAGetLastError();
-
-        close_socket(socket);
-
-        // todo error handling
-        throw std::runtime_error("Invalid socket");
-#elif defined(LINUX)
-        std::string error = "fail to create listen";
-
-        switch (errno) {
-            // todo get error message
-        default:
-            break;
-        }
+        std::string error = _get_last_error("Failed to listen to port " + std::to_string(port) + ".");
 
         throw std::runtime_error(error.c_str());
-#endif
     }
 }
 
@@ -160,21 +364,9 @@ Socket::open()
     SOCKET client = ::accept(socket, nullptr, nullptr);
 
     if(client == INVALID_SOCKET) {
-#ifdef LOSER
-        int error = ::WSAGetLastError();
-        // todo error handling
-        throw std::runtime_error("unable to create client");
-#elif defined(LINUX)
-        std::string error = "fail to create client";
-
-        switch (errno) {
-            // todo get error message
-        default:
-            break;
-        }
+        std::string error = _get_last_error("Failed to accept from client.");
 
         throw std::runtime_error(error.c_str());
-#endif
     }
 
     sockaddr_in client_info     = {};
@@ -187,11 +379,9 @@ Socket::open()
 #endif
 
     if (getpeername_result == SOCKET_ERROR) {
-#ifdef LOSER
-        int error = ::WSAGetLastError();
-        // todo error handling
-#endif
-        throw std::runtime_error("unable to get name");
+        std::string error = _get_last_error("Faild to get et client info.");
+
+        throw std::runtime_error(error.c_str());
     }
 
     std::string client_ip = ::inet_ntoa(client_info.sin_addr);
@@ -207,8 +397,6 @@ Socket::open()
                            "Content-Length: " + std::to_string(body.size()) + "\r\n\r\n" +
                            body.c_str();
 
-    std::cout << "sending response" << std::endl;
-
     ::send(client, response.c_str(), response.size(), 0);
     close_socket(client);
 }
@@ -216,8 +404,6 @@ Socket::open()
 void
 Socket::close()
 {
-    std::cout << "closing socket" << std::endl;
-
     close_socket(socket);
 
     is_open = false;
@@ -233,21 +419,10 @@ Socket::close_socket(SOCKET s)
 #endif
 
     if(shutdown_result == SOCKET_ERROR) {
-        // todo error handling
-#ifdef LOSER
-        int error = ::WSAGetLastError();
-        throw std::runtime_error("unable to shutdown socket");
-#elif defined(LINUX)
-        std::string error = "fail to shutdown client";
+        std::string error = _get_last_error("Failed to shutdown connection.");
 
-        switch (errno) {
-            // todo get error message
-        default:
-            break;
-        }
-
-        throw std::runtime_error(error.c_str());
-#endif
+        std::cerr << error << std::endl;
+        // throw std::runtime_error(error.c_str());
     }
 
 #ifdef LOSER
