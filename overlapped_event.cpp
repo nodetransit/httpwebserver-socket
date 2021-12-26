57bea3fe154c3656d19f6bed798d797bf0ed4059
frostbane <frostbane@programmer.net>
2021-12-26 17:52:18 +1000

    create overlapped event class

---
+++ overlapped_event.cpp
@@ -0,0 +1,87 @@
+#include "overlapped_event.hpp"
+
+#include "common.hpp"
+#include "interfaces/socket.hpp"
+#include "timeval.hpp"
+
+#include <macros/leave_loop_if.hpp>
+#include <macros/scope_guard.hpp>
+#include <macros/repeat_until.hpp>
+
+using namespace nt::http;
+
+
+#ifdef LOSE
+OverlappedEvent::OverlappedEvent() :
+      handle(_handle),
+      _handle(INVALID_HANDLE_VALUE),
+      overlapped(_overlapped),
+      _overlapped({0}),
+      socket(nullptr)
+{
+    _handle = ::CreateEvent(nullptr, true, false, nullptr);
+
+    if (_handle == INVALID_HANDLE_VALUE) {
+        throw std::runtime_error("Failed to create event.");
+    }
+
+    _overlapped.hEvent = _handle;
+}
+
+OverlappedEvent::OverlappedEvent(RawSocket* socket) :
+      handle(_handle),
+      _handle(INVALID_HANDLE_VALUE),
+      overlapped(_overlapped),
+      _overlapped({0}),
+      socket(socket)
+{
+    _handle = ::WSACreateEvent();
+
+    if (_handle == WSA_INVALID_EVENT) {
+        throw std::runtime_error("Failed to create event.");
+    }
+}
+
+OverlappedEvent::~OverlappedEvent()
+{
+    if (_handle != INVALID_HANDLE_VALUE) {
+        ::CloseHandle(_handle);
+    }
+}
+
+void
+OverlappedEvent::set()
+{
+    bool is_pipe = socket == nullptr;
+
+    if (is_pipe) {
+        ::SetEvent(_overlapped.hEvent);
+    } else {
+        int select_result = ::WSAEventSelect(socket->socket,
+                                             _handle,
+                                             FD_ACCEPT | FD_READ // | FD_WRITE | FD_CLOSE
+                                            );
+
+        if (select_result == SOCKET_ERROR) {
+            throw std::runtime_error("Failed to select WSA event.");
+        }
+
+    }
+}
+
+void
+OverlappedEvent::reset()
+{
+    bool is_pipe = socket == nullptr;
+
+    if (is_pipe) {
+        if (!::ResetEvent(_handle)) {
+            throw std::runtime_error("Unable to reset event.");
+        }
+    } else {
+        if (!::WSAResetEvent(_handle)) {
+            throw std::runtime_error("Unable to reset wsa event.");
+        }
+    }
+}
+#endif
---
+++ overlapped_event.hpp
@@ -0,0 +1,34 @@
+#ifndef HTTPWEBSERVER_OVERLAPPED_EVENT_SOCKET_HPP__
+#define HTTPWEBSERVER_OVERLAPPED_EVENT_SOCKET_HPP__
+
+#include "common.hpp"
+#include "raw_socket.hpp"
+#include "interfaces/socket.hpp"
+
+namespace nt { namespace http {
+
+
+class __HttpWebServerSocketPort__ OverlappedEvent
+{
+#ifdef LOSE
+public:
+    const HANDLE    & handle;
+    const OVERLAPPED& overlapped;
+private:
+    HANDLE _handle;
+    OVERLAPPED _overlapped;
+    OVERLAPPED* __overlapped;
+    RawSocket * socket;
+public:
+    OverlappedEvent();
+    OverlappedEvent(RawSocket*);
+    ~OverlappedEvent();
+
+    void set();
+    void reset();
+#endif
+};
+
+}}
+
+#endif /* HTTPWEBSERVER_OVERLAPPED_EVENT_SOCKET_HPP__ */
